94bb4dc45e87e8ea0afe74236b91362f573a366b
obertens <obertens@gmail.com>
2021-04-27 20:29:22 -0400

    Fundamentos Basicos 1 JS

--- 2.js
+++ 2.js
@@ -1,1 +1,9 @@
-function
+    function pares() {
+        var sum = 0;
+        for (var i = 1; i<=1000 ; i++) {
+            if (i % 2 == 0)
+            sum = sum + i
+        }
+        return sum;
+    }
+    console.log(pares());
