0605909e75a74f42538f53fa3c5c79ae4a2ceeb2
obertens <obertens@gmail.com>
2021-04-27 20:18:22 -0400

    Fundamentos Basicos 1 JS

---
+++ 1.js
@@ -0,0 +1,9 @@
+    function contar() {
+        var array = [];
+        for (var i = 1 ; i <= 255; i++){
+        array.push(i);
+        }
+        return array;
+    }
+    console.log(contar());
+    
